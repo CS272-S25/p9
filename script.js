d734ba126fbdc799affa323695a522a4c3146701
Emilia Cantero <149899425+ecaantero@users.noreply.github.com>
2025-04-17 15:15:23 -0500

    Update script.js
    alert on contact us form

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// alerts users that the message is sent in contact us form 
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const message = document.getElementById('form-message');
+    message.classList.remove('hidden');
+    this.reset();
+    setTimeout(() => message.classList.add('hidden'), 3000);
+});
+
+// 
